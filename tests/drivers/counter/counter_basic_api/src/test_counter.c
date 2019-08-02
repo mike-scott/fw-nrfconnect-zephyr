nrfconnect/sdk-zephyr

[nrf toup] tests: drivers: counter: Add clock stabilization nRF
Xtal LF clock source starts hundreds of milliseconds. When it is
not start, test may fail due to wrong timing. Added pending
on LF clock being start in test setup.

Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

tests/drivers/counter/counter_basic_api/src/test_counter.c
@@ -7,6 +7,10 @@
 #include <counter.h>
 #include <ztest.h>
 #include <kernel.h>
+#ifdef CONFIG_SOC_FAMILY_NRF
+#include <drivers/clock_control/nrf_clock_control.h>
+#include <clock_control.h>
+#endif
 
 static volatile u32_t top_cnt;
 static volatile u32_t alarm_cnt;
@@ -81,6 +85,16 @@ typedef void (*counter_test_func_t)(const char *dev_name);
 
 static void counter_setup_instance(const char *dev_name)
 {
+#ifdef CONFIG_SOC_FAMILY_NRF
+	struct device *clock =
+			device_get_binding(DT_NORDIC_NRF_CLOCK_0_LABEL "_32K");
+
+	__ASSERT_NO_MSG(clock);
+
+	while (clock_control_on(clock, (void *)CLOCK_CONTROL_NRF_K32SRC) != 0) {
+		/* empty */
+	}
+#endif
 	alarm_cnt = 0U;
 }
 

mike-scott/fw-nrfconnect-zephyr

Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
(cherry picked from commit 94b7d5affeea5df700c2aa1e3329292fa3b8e8eb)
(cherry-picked from commit 7cf799102d9a35781cc09b2f1d515ec2386ad1c4)

tests/drivers/counter/counter_basic_api/src/test_counter.c
@@ -7,6 +7,10 @@
 #include <drivers/counter.h>
 #include <ztest.h>
 #include <kernel.h>
+#ifdef CONFIG_SOC_FAMILY_NRF
+#include <drivers/clock_control/nrf_clock_control.h>
+#include <clock_control.h>
+#endif
 
 static volatile u32_t top_cnt;
 static volatile u32_t alarm_cnt;
@@ -81,6 +85,16 @@ typedef void (*counter_test_func_t)(const char *dev_name);
 
 static void counter_setup_instance(const char *dev_name)
 {
+#ifdef CONFIG_SOC_FAMILY_NRF
+	struct device *clock =
+		device_get_binding(DT_INST_0_NORDIC_NRF_CLOCK_LABEL "_32K");
+
+	__ASSERT_NO_MSG(clock);
+
+	while (clock_control_on(clock, (void *)CLOCK_CONTROL_NRF_K32SRC) != 0) {
+		/* empty */
+	}
+#endif
 	alarm_cnt = 0U;
 }
 